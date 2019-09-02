b7fdc9241fc263207658328c5ea7e4494d2dd7d6
Bhardwaj Chittoor Ganesh Kumar <cgkbharadwaj@gmail.com>
2019-09-02 22:11:06 +0100

    ansiblefinal

--- Ansible_Setup/Setup_Ansible.sh
+++ Ansible_Setup/Setup_Ansible.sh
@@ -16,34 +16,3 @@ echo "\**** After Installation"
 echo  $ansible_version
 echo "\****"
 apt show ansible
-
-export AWS_ACCESS_KEY_ID='XXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-export AWS_SECRET_ACCESS_KEY='XXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-
-cd /etc/ansible/
-sudo wget https://raw.githubusercontent.com/cgkbharadwaj/ansible_config/master/ec2.ini
-sudo wget https://raw.githubusercontent.com/cgkbharadwaj/ansible_config/master/ec2.py
-
-export ANSIBLE_HOSTS=/etc/ansible/ec2.py
-export EC2_INI_PATH=/etc/ansible/ec2.ini
-
-
-#sudo apt install python-pip
-#sudo apt install python3-pip
-#sudo pip3 install boto
-cd /etc/ansible
-sudo chmod +x ec2.ini
-sudo chmod +x ec2.py
-/etc/ansible/ec2.py --list
-ssh-agent bash
-
-cd /home/ubuntu
-cd aws_doc_steps/
-wget https://raw.githubusercontent.com/cgkbharadwaj/awsKeypai/master/terraformworkload.pem
-ssh-add /home/ubuntu/aws_doc_steps/terraformworkload.pem
-
-cd /home/ubuntu
-mkdir ansible_control_center
-gitclone https://github.com/ansible/ansible-examples.git
-cd ansible-examples
-ls
---
+++ Ansible_Setup/Setup_Ansible_part2.sh
@@ -0,0 +1,24 @@
+cd /etc/ansible/
+sudo wget https://raw.githubusercontent.com/cgkbharadwaj/ansible_config/master/ec2.ini
+sudo wget https://raw.githubusercontent.com/cgkbharadwaj/ansible_config/master/ec2.py
+
+export ANSIBLE_HOSTS=/etc/ansible/ec2.py
+export EC2_INI_PATH=/etc/ansible/ec2.ini
+
+cd /etc/ansible
+sudo chmod +x ec2.ini
+sudo chmod +x ec2.py
+/etc/ansible/ec2.py --list
+ssh-agent bash
+
+cd /home/ubuntu
+sudo mkdir aws_doc_steps
+cd aws_doc_steps/
+sudo wget https://raw.githubusercontent.com/cgkbharadwaj/awsKeypai/master/terraformworkload.pem
+ssh-add /home/ubuntu/aws_doc_steps/terraformworkload.pem
+
+cd /home/ubuntu
+sudo mkdir ansible_control_center
+sudo git clone https://github.com/ansible/ansible-examples.git
+cd ansible-examples/
+ls
