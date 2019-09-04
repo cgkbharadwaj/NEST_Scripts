05a6d35eaeb29343ec1f16ad48e5cd4e86cdb885
Bhardwaj Chittoor Ganesh Kumar <cgkbharadwaj@gmail.com>
2019-09-04 10:30:37 +0100

    last commit

--- Ansible/run_Ansible/MySQL_Setup/Install_SQL.sh
+++ Ansible/run_Ansible/MySQL_Setup/Install_SQL.sh
@@ -18,6 +18,9 @@ sudo mysql_secure_installation
 sudo systemctl status mysql
 
 sudo mysql -u root -p
+SELECT user,authentication_string,plugin,host FROM mysql.user;
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'very_strong_password';
+SELECT user,authentication_string,plugin,host FROM mysql.user;
 
 FLUSH PRIVILEGES;
 
